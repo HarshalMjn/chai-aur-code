fcd417f789b0cb3520de92dcb66002dd45733153
HarshalMjn <mahajanh006@gmail.com>
2023-12-27 12:22:12 +0530

    Create own react library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,10 +4,16 @@ import Chai from "./Chai";
 
 
 function App() {
+  const username = "chai our code !"
  
 
   return (
+    <>
     <Chai/>
+    <h1>chai our react {username}</h1>
+    <p>test para</p>
+    </>
+    
   )
 }
 
